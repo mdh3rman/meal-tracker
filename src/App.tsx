9302cac3820d27ee8ea01f8b0775187217f4c024
Herman <h3rman87@gmail.com>
2026-05-31 15:08:59 +0800

    feat: add App shell with page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { useState } from 'react'
+import { DayView } from './pages/DayView'
+import { History } from './pages/History'
+import { Goals } from './pages/Goals'
+import { BottomNav } from './components/BottomNav'
+import type { Page } from './types'
+
 export default function App() {
-  return <div />
+  const [page, setPage] = useState<Page>('day')
+  const [selectedDate, setSelectedDate] = useState<string | null>(null)
+
+  function goToDay(date: string) {
+    setSelectedDate(date)
+    setPage('day')
+  }
+
+  return (
+    <div className="max-w-md mx-auto relative">
+      {page === 'day' && <DayView initialDate={selectedDate ?? undefined} />}
+      {page === 'history' && <History onDaySelect={goToDay} />}
+      {page === 'goals' && <Goals />}
+      <BottomNav current={page} onChange={setPage} />
+    </div>
+  )
 }
